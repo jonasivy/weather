c32f0c41fef3cbd343f6ddf61df6c01c008c2eb9
Jonas Ivy Imperial <jonasivyimperial@gmail.com>
2023-09-18 00:44:18 +0800

    :hammer: Added DB query logging at app service provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('DB_QUERY_LOG')) {
+            DB::listen(function ($query) {
+                Log::channel('database-query')
+                    ->info($query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL);
+            });
+        }
     }
 }
